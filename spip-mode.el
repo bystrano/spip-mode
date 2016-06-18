93cc4983a2b50a1af42e15b4b6e90e1014baba49
Michel Bystranowski <bystrano@gmx.ch>
2016-06-18 12:52:27 +0200

    commit initial - déclaration d'intentions…

---
+++ spip-mode.el
@@ -0,0 +1,21 @@
+;;; spip-mode.el --- Un mode mineur pour SPIP
+
+;;; Commentary:
+
+;;; Un mode mineur pour le CMS SPIP (http://spip.net).
+
+;;; TODO:
+
+;;; Configure web-mode, php-mode et javascript mode pour le CMS
+;;; SPIP. PHP-mode utilise les règles de codage SPIPiennes,
+;;; web-mode colore les boucles, filtres et balise SPIP.
+
+;;; Fournit des commandes pour la gestion des chaînes de langue
+
+;;; Fournit des commandes pour la gestion des surcharges de
+;;; fichiers du core.
+
+;;; Code:
+
+(provide spip-mode)
+;;; spip-mode.el ends here
