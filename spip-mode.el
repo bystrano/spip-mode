df2e52fe5099d7cdcccfbbd238149ac9f1dce6cc
Michel Bystranowski <bystrano@gmx.ch>
2016-06-19 12:12:11 +0200

    trouver la racine, et configurer php-mode et web-mode

--- spip-mode.el
+++ spip-mode.el
@@ -4,11 +4,11 @@
 
 ;;; Un mode mineur pour le CMS SPIP (http://spip.net).
 
-;;; TODO:
-
 ;;; Configure web-mode, php-mode et javascript mode pour le CMS
 ;;; SPIP. PHP-mode utilise les règles de codage SPIPiennes,
 ;;; web-mode colore les boucles, filtres et balise SPIP.
+
+;;; TODO:
 
 ;;; Fournit des commandes pour la gestion des chaînes de langue
 
@@ -16,6 +16,53 @@
 ;;; fichiers du core.
 
 ;;; Code:
+
+(defvar spip-root nil
+  "The root directory of the current SPIP project.")
+
+(define-minor-mode spip-mode
+  "A minor mode for the SPIP CMS."
+  (if (not (stringp spip-root))
+      (setq spip-root (get-spip-root)))
+  ;; If not in a SPIP project, get out.
+  (if (not (stringp spip-root))
+      (setq spip-mode nil)))
+
+(defun spip-mode-web-mode-config ()
+  "Configures web-mode for SPIP."
+  (spip-mode)
+  (if (spip-mode)
+      (web-mode-set-engine "spip")))
+
+(defun spip-mode-php-mode-config ()
+  "Configures php-mode for SPIP."
+  (spip-mode))
+
+(defun get-spip-root ()
+  "Return the root of the current SPIP project.
+Returns nil if not in a SPIP project."
+
+  (let* ((path (directory-file-name
+               (expand-file-name default-directory)))
+         (parents (list path)))
+
+    (while (not (string= path ""))
+      (setq path
+            (mapconcat 'identity
+                       (reverse (cdr (reverse (split-string path "/"))))
+                       "/"))
+      (add-to-list 'parents path))
+
+    (spip-get-directory (locate-file "spip.php" parents))))
+
+(defun spip-get-directory (filename)
+  "Returns the directory component of FILENAME."
+
+  (if (stringp filename)
+      (directory-file-name
+       (mapconcat  'identity
+                   (reverse (cdr (reverse (split-string filename "/"))))
+                   "/"))))
 
 (provide spip-mode)
 ;;; spip-mode.el ends here
