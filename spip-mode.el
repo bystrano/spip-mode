0b51bdb07fa8048531bb6eba6b7aeabdcc0e1add
Michel Bystranowski <bystrano@gmx.ch>
2016-07-21 13:04:36 +0200

    la variable spip-root doit être définie comme locale

--- spip-mode.el
+++ spip-mode.el
@@ -36,7 +36,11 @@
 (define-minor-mode spip-mode
   "A minor mode for the SPIP CMS."
   nil " SPIP " nil
+
+  (make-local-variable 'spip-root)
+
   (setq spip-root (get-spip-root))
+
   ;; If not in a SPIP project, get out.
   (if (not (stringp spip-root))
       (setq spip-mode nil)))
